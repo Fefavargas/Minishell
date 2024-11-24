797166597dd190021e9571588382aad8512f6994
Janae Byrne <janaebyrne@gmail.com>
2024-11-24 01:44:38 +0100

    cleaned up execute commands function, added to notes

--- srcs/execution/execution.c
+++ srcs/execution/execution.c
@@ -6,28 +6,12 @@
 /*   By: janaebyrne <janaebyrne@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 18:08:48 by fvargas           #+#    #+#             */
-/*   Updated: 2024/11/24 00:10:37 by janaebyrne       ###   ########.fr       */
+/*   Updated: 2024/11/24 01:42:14 by janaebyrne       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int is_builtin(const char *cmd)
-{
-    if (ft_strncmp(cmd, "echo", 4) == 0)
-        return 1;
-    if (ft_strncmp(cmd, "cd", 2) == 0)
-        return 1;
-    if (ft_strncmp(cmd, "pwd", 3) == 0)
-        return 1;
-    if (ft_strncmp(cmd, "env", 3) == 0)
-        return 1; 
-    if (ft_strncmp(cmd, "unset", 5) == 0)
-        return 1;
-    if (ft_strncmp(cmd, "export", 6) == 0) 
-        return 1;  
-    return 0; 
-}
 void	execute_commands(t_mini shell)
 {
 	int saved_stdout;
@@ -40,23 +24,18 @@ void	execute_commands(t_mini shell)
 	{
 		setup_redirection(&shell);
 	}
-	if (is_builtin(shell.cmd))
-	{
-		if (ft_strncmp(shell.cmd, "echo", 4) == 0)
-		{
-			ft_echo(shell.cmd, shell.str);
-		}
-		else if (ft_strncmp(shell.cmd, "cd", 2) == 0)
-			ft_cd(shell.str);
-		else if (ft_strncmp(shell.cmd, "pwd", 3) == 0)
-			ft_pwd();
-		else if (ft_strncmp(shell.cmd, "env", 3) == 0)
-			ft_env(shell.local_envp);
-		else if (ft_strncmp(shell.cmd, "unset", 5) == 0)
-			ft_unset(shell.str, shell.local_envp);
-		else if (ft_strncmp(shell.cmd, "export", 6) == 0)
-			ft_export(shell.str, shell.file, shell.local_envp);
-	}
+	if (ft_strncmp(shell.cmd, "echo", 4) == 0)
+		ft_echo(shell.cmd, shell.str);
+	else if (ft_strncmp(shell.cmd, "cd", 2) == 0)
+		ft_cd(shell.str);
+	else if (ft_strncmp(shell.cmd, "pwd", 3) == 0)
+		ft_pwd();
+	else if (ft_strncmp(shell.cmd, "env", 3) == 0)
+		ft_env(shell.local_envp);
+	else if (ft_strncmp(shell.cmd, "unset", 5) == 0)
+		ft_unset(shell.str, shell.local_envp);
+	else if (ft_strncmp(shell.cmd, "export", 6) == 0)
+		ft_export(shell.str, shell.file, shell.local_envp);
 	else
 		ft_execute(shell.cmd, shell.local_envp);
 	dup2(saved_stdin, STDIN_FILENO);
